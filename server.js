ff96a7a76399b5e1d50e123fedd4d0ec572422c9
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-10 16:20:02 -0700

    Initial re-copy of sandbox to fix cursor problems

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const app = require('./lib/app');
 const server = http.createServer(app);
 const io = new Server(server, { cors: true });
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 8080;
 
 const { users, addUser, deleteUser } = require('./lib/socket/user-utils');
 
-const onConnection = socket => {
+const onConnection = (socket) => {
   addUser(socket);
+
   socket.join(`${users[socket.id].room}`);
 
-  socket.on('movement', data => {
+  socket.on('movement', (data) => {
     data.id = socket.id;
-    socket.broadcast.to(`${users[socket.id]}.room`).emit('moving', data);
+    socket.broadcast.to(`${users[socket.id].room}`).emit('moving', data);
   });
 
   socket.on('disconnect', () => {
@@ -23,11 +24,11 @@ const onConnection = socket => {
     deleteUser(socket);
   });
 
-  socket.on('click', buttonState => {
+  socket.on('click', (buttonState) => {
     socket.broadcast.to(`${users[socket.id].room}`).emit('click', buttonState);
   });
 
-  socket.on('client message', (message) => {
+  socket.on('client message', message => {
     socket.to(`${users[socket.id].room}`).emit('socket message', message);
   });
 };
@@ -35,5 +36,6 @@ const onConnection = socket => {
 io.sockets.on('connection', onConnection);
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
