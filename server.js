73c31a309091a76d1feaf337463106d6c23a36e4
devon-wolf <33987744+devon-wolf@users.noreply.github.com>
2021-06-11 15:35:35 -0700

    Feature/online users (#4)
    * Small addition to join event so number of online users can be easily broadcast.
    
    * Small addition to join event so number of online users can be easily broadcast.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const onConnection = socket => {
   // on every connection, add a user a join that user's socket to the current room
   addUser(socket);
   socket.join(`${users[socket.id].room}`);
+  socket.broadcast.to(`${users[socket.id].room}`).emit('new user', socket.id);
 
   // take in cursor movement data and broadcast to other clients
   const onMovement = movementData => {
@@ -54,3 +55,4 @@ io.sockets.on('connection', onConnection);
 server.listen(PORT, () => {
   console.log(`Started on ${PORT}`);
 });
+
